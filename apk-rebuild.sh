3cc9c210172e4a5e51d306ddb5c35fb53c072ba2
Illia Kozyr <names.ilya@gmail.com>
2021-11-06 11:41:29 +0200

    applied some of best practises

--- apk-rebuild.sh
+++ apk-rebuild.sh
@@ -27,23 +27,23 @@ set_path() {
 
 check_tools() {
 	have_all_tools=true
-	if [ ! -f $bundletool_path ]; then
+	if [ ! -f "$bundletool_path" ]; then
 		echo "${RED}Bundletool not found, check the path in the script or download Bundletool at https://github.com/google/bundletool/releases"
 		have_all_tools=false
 	fi
-	if [ ! -f $apktool_path ]; then
+	if [ ! -f "$apktool_path" ]; then
 		echo "${RED}Apktool not found, check the path in the script or download Apktool at https://github.com/iBotPeaches/Apktool/releases"
 		have_all_tools=false
 	fi
-	if [ ! -f $apk_signer_path ]; then
+	if [ ! -f "$apk_signer_path" ]; then
 		echo "${RED}Apk_signer not found, check the path in the script or download Apk_signer at https://github.com/patrickfav/uber-apk-signer/releases"
 		have_all_tools=false
 	fi
-	if [[ `which xmlstarlet` = "" ]]; then
+	if [[ $(which xmlstarlet) = "" ]]; then
 		echo "${RED}xmlstarlet not found, install it via 'brew install xmlstarlet'"
 		have_all_tools=false
 	fi
-	if [[ ! -f $keystore_path ]]; then
+	if [[ ! -f "$keystore_path" ]]; then
 		echo "${RED}keystore file not found, specify the path in the script or install Android Studio"
 		have_all_tools=false
 	fi
@@ -53,7 +53,7 @@ check_tools() {
 }
 
 print_usage() {
-	script_name=`basename "$0"`
+	script_name=$(basename "$0")
 	echo -e "${BLACK}USAGE${NC}"
 	echo -e "\t$script_name file [OPTIONS]"
 	echo -e
@@ -78,7 +78,7 @@ array_has_elem () {
 	[[ ${array[*]} =~ (^|[[:space:]])"$elem"($|[[:space:]]) ]] && echo 1 || echo 0
 }
 
-run () {
+main () {
 	SECONDS=0
 
 	set_vars
@@ -98,7 +98,7 @@ run () {
 
 	if [[ $file_ext = "aab" ]]; then
 		echo "${CYAN}Extracting apks from aab${NC}"
-		java -jar $bundletool_path build-apks --bundle="$file_name.$file_ext" --output="$file_name.apks" --mode=universal
+		java -jar "$bundletool_path" build-apks --bundle="$file_name.$file_ext" --output="$file_name.apks" --mode=universal
 
 		echo "${CYAN}Extracting apk from apks and moving it${NC}"
 		unzip "$file_name.apks" -d "$file_name"
@@ -115,8 +115,8 @@ run () {
 	decompiled_path="$file_dir/$file_name.$file_ext-decompiled"
 
 	echo "${CYAN}Decompiling the APK file${NC}"
-	java -jar $apktool_path d -o "$decompiled_path" "$file_dir/$file_name.apk"
-	# java -jar $apktool_path d --only-main-classes -o "$decompiled_path" "$file_dir/$file_name.apk"
+	java -jar "$apktool_path" d -o "$decompiled_path" "$file_dir/$file_name.apk"
+	# java -jar "$apktool_path" d --only-main-classes -o "$decompiled_path" "$file_dir/$file_name.apk"
 
 	nsc_file="$decompiled_path/res/xml/network_security_config.xml"
 	echo "${CYAN}Processing ${YELLOW}@xml/network_security_config.xml${NC}"
@@ -136,39 +136,39 @@ run () {
 		echo "</network-security-config>" >> "$nsc_file"
 	fi
 
-	if [[ `xmlstarlet sel -t -c "/network-security-config/base-config" "$nsc_file"` = "" ]]; then
+	if [[ $(xmlstarlet sel -t -c "/network-security-config/base-config" "$nsc_file") = "" ]]; then
 		xmlstarlet ed --inplace -s "/network-security-config" -t elem -n base-config "$nsc_file"
 		xmlstarlet ed --inplace -a "/network-security-config/base-config" -t attr -n cleartextTrafficPermitted -v true "$nsc_file"
-	elif [[ `xmlstarlet sel -t -c "/network-security-config/base-config[@cleartextTrafficPermitted='true']" "$nsc_file"` = "" ]]; then
+	elif [[ $(xmlstarlet sel -t -c "/network-security-config/base-config[@cleartextTrafficPermitted='true']" "$nsc_file") = "" ]]; then
 		xmlstarlet ed --inplace -a "/network-security-config/base-config" -t attr -n cleartextTrafficPermitted -v true "$nsc_file"
 	fi
 
-	if [[ `xmlstarlet sel -t -c "/network-security-config/base-config/trust-anchors" "$nsc_file"` = "" ]]; then
+	if [[ $(xmlstarlet sel -t -c "/network-security-config/base-config/trust-anchors" "$nsc_file") = "" ]]; then
 		xmlstarlet ed --inplace -s "/network-security-config/base-config" -t elem -n trust-anchors "$nsc_file"
 	fi
 
 	for ca_type in "system" "user"; do
-		if [[ `xmlstarlet sel -t -c "/network-security-config/base-config/trust-anchors/certificates[@src='$ca_type']" "$nsc_file"` = "" ]]; then
+		if [[ $(xmlstarlet sel -t -c "/network-security-config/base-config/trust-anchors/certificates[@src='$ca_type']" "$nsc_file") = "" ]]; then
 			xmlstarlet ed --inplace -s "/network-security-config/base-config/trust-anchors" -t elem -n certificates "$nsc_file"
 			xmlstarlet ed --inplace -a "/network-security-config/base-config/trust-anchors/certificates[not(@src)]" -t attr -n src -v $ca_type "$nsc_file"
 		fi	
 	done
 
 	echo "${CYAN}Checking ${YELLOW}AndroidManifest.xml${NC}"
-	if [[ `xmlstarlet sel -t -c "/manifest/application[@android:networkSecurityConfig='@xml/network_security_config']" "$decompiled_path/AndroidManifest.xml"` = "" ]]; then
+	if [[ $(xmlstarlet sel -t -c "/manifest/application[@android:networkSecurityConfig='@xml/network_security_config']" "$decompiled_path/AndroidManifest.xml") = "" ]]; then
 		xmlstarlet ed --inplace -a "/manifest/application" -t attr -n android:networkSecurityConfig -v @xml/network_security_config "$decompiled_path/AndroidManifest.xml"
 	fi
 
-	if [[ `array_has_elem "$*" "--pause"` = 1 ]]; then
+	if [[ $(array_has_elem "$*" "--pause") = 1 ]]; then
 		echo "${CYAN}Paused. Perform necessary actions and press any key to continue ${NC}"
 		read
 	fi
 
 	echo "${CYAN}Building new APK file${NC}"
-	java -jar $apktool_path b "$decompiled_path" --use-aapt2
+	java -jar "$apktool_path" b "$decompiled_path" --use-aapt2
 
 	echo "${CYAN}Signing APK file${NC}"
-	java -jar $apk_signer_path -a "$decompiled_path/dist/$file_name.apk" --allowResign --overwrite
+	java -jar "$apk_signer_path" -a "$decompiled_path/dist/$file_name.apk" --allowResign --overwrite
 
 	mv "$decompiled_path/dist/$file_name.apk" "$decompiled_path.apk"
 	if [[ $file_ext = "aab" ]]; then
@@ -177,19 +177,19 @@ run () {
 
 	echo "${CYAN}Done in $SECONDS seconds${NC}"
 
-	if [[ `array_has_elem "$*" "-r"` = 1 ]] || [[ `array_has_elem "$*" "--remove"` = 1 ]]; then
+	if [[ $(array_has_elem "$*" "-r") = 1 ]] || [[ $(array_has_elem "$*" "--remove") = 1 ]]; then
 		echo "${CYAN}Removing the source file $file_name.$file_ext${NC}"
 		rm "$file_name.$file_ext"
 	fi
 
-	if [[ `array_has_elem "$*" "-p"` = 0 ]] && [[ `array_has_elem "$*" "--preserve"` = 0 ]]; then
+	if [[ $(array_has_elem "$*" "-p") = 0 ]] && [[ $(array_has_elem "$*" "--preserve") = 0 ]]; then
 		echo "${CYAN}Removing the unpacked content of the apk file $decompiled_path${NC}"
 		rm -rf "$decompiled_path"
 	fi
 
-	if [[ `array_has_elem "$*" "-i"` = 1 ]] || [[ `array_has_elem "$*" "--install"` = 1 ]]; then
+	if [[ $(array_has_elem "$*" "-i") = 1 ]] || [[ $(array_has_elem "$*" "--install") = 1 ]]; then
 		echo "${CYAN}Installing the rebuilded apk file ${YELLOW}$decompiled_path.apk${NC}"
-		if [[ `which adb` = "" ]]; then
+		if [[ $(which adb) = "" ]]; then
 			echo "${RED}adb not found, add the path to adb to the PATH env var or install Android Studio or standalone command line tools"
 		else
 			adb install "$decompiled_path.apk"
@@ -199,4 +199,4 @@ run () {
 	echo "${CYAN}Output APK file: ${YELLOW}$decompiled_path.apk${NC}"
 }
 
-run "$@"
+main "$@"
